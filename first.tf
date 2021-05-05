50cf9f2621edd040d0d12443d03f69ce841bcaa5
Aamir Meman <aamirmemon2292@gmail.com>
2021-05-05 18:02:01 -0400

    day 1 - linuxworld

---
+++ first.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region="us-east-1"
+profile="account1"
+}
+
+resource "aws_instance" "os1" {
+ami = "ami-0d5eff06f840b45e9"
+instance_type = "t2.micro"
+tags = {
+  Name = "my first TF OS 11"
+ }
+}
